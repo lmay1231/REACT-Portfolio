71a751f29e32dc4a74b0e1fb2fe07f0b170654da
Logan May <108742258+lmay1231@users.noreply.github.com>
2023-11-26 04:45:18 +0000

    Changed pictures and basic information

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,21 +10,21 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/fountain-background.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "a fountain in the middle of a plaza on a college campus";
 
 /**
- * Sort description that expands on your title on the Home component.
+ * Short description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a computer science student studying at Samford University. I enjoy being creative and designing intuitive and accesible user interfaces.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
